b18a4bf5ed186ff0e1f0dbef5a4beb95b44d97c5
Lucca Radaeli <radaelidev@gmail.com>
2020-06-13 20:58:48 -0300

    Creating Input and Button component

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Text, View } from 'react-native'
 
 import Background from '~/components/Background'
+import Input from '~/components/Input'
+import Button from '~/components/Button'
 
 export default function SignUp({navigation}) {
  
   return (      
     <Background>      
       <Text>CADASTRO</Text>
+      <Input style={{marginTop: 30}} icon='call' placeholder="Digite seu nome"></Input>
+      <Button>Entrar</Button>
     </Background>    
   )
 }
