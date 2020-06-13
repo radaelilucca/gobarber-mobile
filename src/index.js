9211f55a37716335e4ce4f435dd1988352f91569
Lucca Radaeli <radaelidev@gmail.com>
2020-06-13 18:27:49 -0300

    Setup RootImport

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 // import { Container } from './styles';
+
 
 export default function App(){
   return <View/>
